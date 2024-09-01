6ae7336624f5e1fa4fa72354c855b4915d2ff620
Rodrigo Suguimoto <rodrigosuguimoto@gmail.com>
2024-09-01 12:33:12 -0300

    Finished stage 3

--- Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
+++ Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
@@ -5,17 +5,38 @@ import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int waterStock = scanner.nextInt();
+        int cupsOfCoffeeWithWaterStock = waterStock / 200;
+
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milkStock = scanner.nextInt();
+        int cupsOfCoffeeWithMilkStock = milkStock / 50;
+
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int coffeeBeansStock = scanner.nextInt();
+        int cupsOfCoffeeWithCoffeeBeansStock = coffeeBeansStock / 15;
+
+        // The maximum amount of cups of coffee
+        int maxCupsOfCoffee = minBetweenThree(cupsOfCoffeeWithWaterStock,
+                cupsOfCoffeeWithMilkStock,
+                cupsOfCoffeeWithCoffeeBeansStock);
+
         System.out.println("Write how many cups of coffee you will need:");
         int cupsOfCoffee = scanner.nextInt();
-        System.out.printf("For %d of coffee you will need:\n", cupsOfCoffee);
+        if (cupsOfCoffee == maxCupsOfCoffee) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (cupsOfCoffee < maxCupsOfCoffee) {
+            int additionalCupsOfCoffee = maxCupsOfCoffee - cupsOfCoffee;
+            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)\n", additionalCupsOfCoffee);
+        } else {
+            System.out.printf("No, I can make only %d cup(s) of coffee\n", maxCupsOfCoffee);
+        }
+    }
 
-        int waterNeeded = cupsOfCoffee * 200;
-        int milkNeeded = cupsOfCoffee * 50;
-        int coffeeBeansNeeded = cupsOfCoffee * 15;
-
-        System.out.printf("%d ml of water\n", waterNeeded);
-        System.out.printf("%d ml of milk\n", milkNeeded);
-        System.out.printf("%d g of coffee beans\n", coffeeBeansNeeded);
+    private static int minBetweenThree(int a, int b, int c) {
+        return Math.min(Math.min(a, b), c);
     }
 
 }
